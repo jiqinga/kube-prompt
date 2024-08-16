6a60fa91b022c0a3ff84960097b9b7a04bbf0792
jiqinga <1758812645@qq.com>
2024-08-16 16:58:38 +0800

    :wrench: 移动internal目录到_tools工具目录

---
+++ _tools/go.mod
@@ -0,0 +1,24 @@
+module _tools
+
+go 1.18
+
+require (
+	github.com/jiqinga/kube-prompt v0.0.0-20240816065146-4e0d74a94657
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/volcengine/volcengine-go-sdk v1.0.152
+)
+
+require (
+	github.com/c-bata/go-prompt v0.2.6 // indirect
+	github.com/c-bata/kube-prompt v1.0.11 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mattn/go-tty v0.0.3 // indirect
+	github.com/pkg/term v1.2.0-beta.2 // indirect
+	github.com/volcengine/volc-sdk-golang v1.0.23 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
---
+++ _tools/internal/optionconv/conv.go
@@ -0,0 +1,79 @@
+package optionconv
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"_tools/ai"
+
+	"github.com/c-bata/go-prompt"
+)
+
+func GetOptionsFromHelpText(help string) (options string, err error) {
+	x := strings.Split(help, "\nOptions:")
+	if len(x) < 2 {
+		return "", errors.New("parse error")
+	}
+	y := strings.Split(x[1], "\n\n")
+	options = strings.Trim(y[0], "\n")
+	return options, nil
+}
+
+func SplitOptions(options string) []string {
+	lines := strings.Split(options, "\n")
+	results := make([]string, 0, len(lines))
+	var index int
+	for _, l := range lines {
+		if strings.HasPrefix(l, "  ") || index == 0 {
+			results = append(results, strings.TrimSpace(l))
+			index++
+		} else {
+			results[index-1] += " " + l
+		}
+	}
+	return results
+}
+
+func convertToSuggest(flagLine string) []prompt.Suggest {
+	x := strings.SplitN(flagLine, ": ", 2)
+	key := x[0]
+	description := x[1]
+
+	var keys []string
+	if strings.Contains(key, ", ") {
+		keys = strings.Split(key, ", ")
+	} else {
+		keys = []string{key}
+	}
+	suggests := make([]prompt.Suggest, len(keys))
+	for i := range keys {
+		if strings.Contains(keys[i], "=") {
+			keys[i] = strings.Split(keys[i], "=")[0]
+		}
+		keys[i] = strings.TrimSpace(keys[i])
+		suggests[i] = prompt.Suggest{Text: keys[i], Description: description}
+	}
+	return suggests
+}
+
+func ConvertToSuggestions(options []string) []prompt.Suggest {
+	suggestions := make([]prompt.Suggest, 0, len(options))
+	for i := range options {
+
+		str := strings.Split(options[i], ":")
+		var modelName string = "ep-20240816164009-58vhd"
+		zhhelp, err := ai.Chat(modelName, fmt.Sprintf("请你将我接下来提供的 kubectl --help 的帮助说明内容，翻译为清晰易懂的中文，不需要详细说明。以下是帮助说明原文: %s", str[1]))
+		if err != nil {
+			fmt.Println(err)
+		}
+		str[1] = zhhelp
+		x := convertToSuggest(strings.Join(str, ": "))
+		// x := convertToSuggest(options[i])
+
+		for j := range x {
+			suggestions = append(suggestions, x[j])
+		}
+	}
+	return suggestions
+}
