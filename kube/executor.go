491e762a83139971c6f93a5759ddd0385f496df4
jiqinga <1758812645@qq.com>
2024-09-30 15:18:57 +0800

    :art: 重构执行器函数以支持命名空间切换和自定义配置

--- kube/executor.go
+++ kube/executor.go
@@ -1,17 +1,27 @@
 package kube
 
+import "C"
 import (
 	"bytes"
+	//"bytes"
 	"fmt"
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 
+	//"github.com/hidetatz/kubecolor/printer"
+
+	//"github.com/hidetatz/kubecolor/command"
+	//"github.com/hidetatz/kubecolor/kubectl"
 	"github.com/jiqinga/kube-prompt/internal/debug"
 )
 
+var globalState sync.Map
+
 func Executor(s string) {
 	s = strings.TrimSpace(s)
+
 	if s == "" {
 		return
 	} else if s == "quit" || s == "exit" {
@@ -19,14 +29,54 @@ func Executor(s string) {
 		os.Exit(0)
 		return
 	}
+	if o := strings.Fields(s); o[0] == "set" {
+		if c := strings.Fields(s); c[1] == "ns" || c[1] == "namespace" {
+			tempfile, err := createTempKubeconfig(c[2])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if err != nil {
+				fmt.Println("error", err)
+				os.Exit(1)
+			}
+			// completer := &Completer{} // 假设你已经有了这个实例
+			// completer.namespace = "s[:len(s)-1]"
+			globalState.Store("KUBECONFIG", tempfile)
+			globalState.Store("namespace", c[2])
+			fmt.Printf("成功切换到命名空间: %s\n", c[2])
 
-	cmd := exec.Command("/bin/sh", "-c", "kubectl "+s)
+			return
+		}
+	}
+	env := os.Environ()
+	value, ok := globalState.Load("KUBECONFIG")
+	if ok {
+		// 键 "KUBECONFIG" 存在
+		env = append(env, fmt.Sprintf("KUBECONFIG=%s", value))
+	}
+
+	shell := os.Getenv("SHELL")
+	if shell == "" {
+		shell = "/bin/sh"
+	}
+
+	// 获取当前工作目录
+	pwd, err := os.Getwd()
+	if err != nil {
+		pwd = "/"
+	}
+	fmt.Println(s)
+	cmd := exec.Command(shell, "-c", "kubectl "+s)
+	cmd.Env = env
+	cmd.Dir = pwd
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Got error: %s\n", err.Error())
 	}
+
 	return
 }
 
@@ -47,4 +97,9 @@ func ExecuteAndGetResult(s string) string {
 	}
 	r := string(out.Bytes())
 	return r
+}
+
+// 添加这个新函数
+func GetGlobalState(key string) (interface{}, bool) {
+	return globalState.Load(key)
 }
